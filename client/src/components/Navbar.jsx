e0793c36db0ca97371bc1c961a3d6d0f5e0ed16b
Dilan Madusanka <madusankadilan226@gmail.com>
2024-03-17 01:36:31 +0530

    navbar updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import Logo from '../img/logo.png'
+
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+
+           <Link className='link' to="/?cat=cricket"  >
+            <h6> CRICKET</h6>
+           </Link>
+
+           <Link className='link' to="/?cat=legue"  >
+            <h6> LEGUE</h6>
+           </Link>
+
+
+           <Link className='link' to="/?cat=technology"  >
+            <h6> TECHNOLOGY</h6>
+           </Link>
+
+
+           <Link className='link' to="/?cat=worldcup"  >
+            <h6> WORLD CUP</h6>
+           </Link>
+
+           <Link className='link' to="/?cat=rank"  >
+            <h6> RANK</h6>
+           </Link>
+
+           <span>Dilan</span>
+           <span>Logout</span>
+           <span className='write'>
+
+            <Link className='link' to="/write">Write</Link>
+
+           </span>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
