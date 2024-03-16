9cb52249f2c3253e55f712f5b87bdbe12498e5f8
Dilan Madusanka <madusankadilan226@gmail.com>
2024-03-17 00:07:24 +0530

    react router updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,74 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Single from "./pages/Single"
+import Home from "./pages/Home"
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+ const Layout = ()=>{
+return (
+
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+ }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+
+      {
+
+        path:"/",
+        element: <Home />,
+
+      },
+
+      {
+        path:"/post/:id",
+        element:<Single />,
+      },
+
+      {
+        path:"/write",
+        element:<Write />,
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+
 
 
 function App() {
   return (
     <div className="App">
-      hellow  world!
+      <div className="container">
+      <RouterProvider router={router} />
+
+      </div>
     </div>
   );
 }
